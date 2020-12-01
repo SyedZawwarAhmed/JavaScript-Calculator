51d2da9c996445ac78994e98deff94deec3c1d3b
Syed Zawwar Ahmed <zawwar.ahmed12@gmail.com>
2020-12-01 23:56:43 +0500

    chnages made

--- app.js
+++ app.js
@@ -3,68 +3,66 @@ const calculation = document.getElementById("calculation");
 const nine = document.getElementById("nine");
 const eight = document.getElementById("eight");
 
-var calArr = [];
-
 const cleared = () => {
-    calculation.textContent = '0';
-    calArr = [];
+    calculation.innerText = '0';
+}
+
+function first0() {
+    if (calculation.innerText === '0') {
+        calculation.innerText = '';
+    }
 }
 
 const pressed9 = () => {
-    calArr.push('9');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '9';
 }
-
 const pressed8 = () => {
-    calArr.push('8');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '8';
 }
-
 const pressed7 = () => {
-    calArr.push('7');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '7';
 }
 const pressed6 = () => {
-    calArr.push('6');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '6';
 }
 const pressed5 = () => {
-    calArr.push('5');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '5';
 }
 const pressed4 = () => {
-    calArr.push('4');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '4';
 }
 const pressed3 = () => {
-    calArr.push('3');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '3';
 }
 const pressed2 = () => {
-    calArr.push('2');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '2';
 }
 const pressed1 = () => {
-    calArr.push('1');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '1';
 }
 const pressed0 = () => {
-    calArr.push('0');
-    const calStr = calArr.join("");
-    calculation.innerText = calStr;
+    first0();
+    calculation.innerText += '0';
 }
-
-
-
-
-
+const pressedDec = () => {
+    first0();
+    let i ;
+    for (i=0; i<calculation.innerText.length; i++) {
+        if (calculation.innerText[i] === '.') {
+            break;
+        }
+    }
+    if (i === calculation.innerText.length) {
+        calculation.innerText += '.';
+    }
+}
 
